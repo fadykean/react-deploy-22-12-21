545a6a626e013d911b2a311da6d07e329be49b0a
fadykean <fady.alkean@gmail.com>
2021-12-22 09:59:28 +0200

    Updates

---
+++ precache-manifest.798c2b53c59aad65fda0e7c2857e4f53.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3bd27c3a55e83ebc4f7d524a53ca1d4",
+    "url": "/react-deploy-22-12-21/index.html"
+  },
+  {
+    "revision": "2861ffe7306a53d6bfc7",
+    "url": "/react-deploy-22-12-21/static/css/2.d9c6ee88.chunk.css"
+  },
+  {
+    "revision": "17156c070bffa5df133c",
+    "url": "/react-deploy-22-12-21/static/css/main.36a7b080.chunk.css"
+  },
+  {
+    "revision": "2861ffe7306a53d6bfc7",
+    "url": "/react-deploy-22-12-21/static/js/2.5bdd0a29.chunk.js"
+  },
+  {
+    "revision": "9dcad5d0557c896f197cc690d093af55",
+    "url": "/react-deploy-22-12-21/static/js/2.5bdd0a29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17156c070bffa5df133c",
+    "url": "/react-deploy-22-12-21/static/js/main.d403ab4f.chunk.js"
+  },
+  {
+    "revision": "136df6fd8548bc6ea411",
+    "url": "/react-deploy-22-12-21/static/js/runtime-main.a1ae82be.js"
+  },
+  {
+    "revision": "e5c3d16a4028a7a21362aefce0cbc2b5",
+    "url": "/react-deploy-22-12-21/static/media/1.e5c3d16a.jpg"
+  },
+  {
+    "revision": "0ad4faad90b2c2012a0729fb292b83ba",
+    "url": "/react-deploy-22-12-21/static/media/2.0ad4faad.jpg"
+  },
+  {
+    "revision": "fa3a802899de61963d15b90e0d8ab735",
+    "url": "/react-deploy-22-12-21/static/media/3.fa3a8028.jpg"
+  },
+  {
+    "revision": "4defd52df0c21e94beecf255119c6243",
+    "url": "/react-deploy-22-12-21/static/media/back.4defd52d.svg"
+  },
+  {
+    "revision": "bc2a55639cfefcedbc9c704ece799ed5",
+    "url": "/react-deploy-22-12-21/static/media/call.bc2a5563.png"
+  },
+  {
+    "revision": "e8bf29d12fc0ff70e3d60805313a8443",
+    "url": "/react-deploy-22-12-21/static/media/chat.e8bf29d1.png"
+  },
+  {
+    "revision": "923ad7aa29e540d9fed26b2a1d906df5",
+    "url": "/react-deploy-22-12-21/static/media/hat.923ad7aa.svg"
+  },
+  {
+    "revision": "cd1d1c978d0030f14f1d8a7690eb8398",
+    "url": "/react-deploy-22-12-21/static/media/img-1.cd1d1c97.jpg"
+  },
+  {
+    "revision": "65b53910f670be0b3337cb65bad3d7f6",
+    "url": "/react-deploy-22-12-21/static/media/img-3.65b53910.jpg"
+  },
+  {
+    "revision": "0ec923fc0fe65b0349ffbf93b33fd7c6",
+    "url": "/react-deploy-22-12-21/static/media/img-4.0ec923fc.jpg"
+  },
+  {
+    "revision": "bd11375f25b4d7b599e2065c9e916507",
+    "url": "/react-deploy-22-12-21/static/media/img-6.bd11375f.jpg"
+  },
+  {
+    "revision": "66001cf811f9f101c450edb126e29c63",
+    "url": "/react-deploy-22-12-21/static/media/img-7.66001cf8.jpg"
+  },
+  {
+    "revision": "11abdde0c31967fe44b9f8f241190db9",
+    "url": "/react-deploy-22-12-21/static/media/location.11abdde0.png"
+  },
+  {
+    "revision": "3e4d0fcb55e0a2312bbc33df9f63124d",
+    "url": "/react-deploy-22-12-21/static/media/medical.3e4d0fcb.svg"
+  },
+  {
+    "revision": "f3f6013cb12e5d6975c062d3ad5981b7",
+    "url": "/react-deploy-22-12-21/static/media/restaurant.f3f6013c.svg"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/react-deploy-22-12-21/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/react-deploy-22-12-21/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/react-deploy-22-12-21/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "684bc7bba8b349794be6e63d3555173b",
+    "url": "/react-deploy-22-12-21/static/media/shopping.684bc7bb.svg"
+  },
+  {
+    "revision": "301bd1302f02a7495b11647ab23091f1",
+    "url": "/react-deploy-22-12-21/static/media/star.301bd130.svg"
+  },
+  {
+    "revision": "d14b3ebdab4a74bd532466900c7cec7f",
+    "url": "/react-deploy-22-12-21/static/media/star_outline.d14b3ebd.svg"
+  },
+  {
+    "revision": "9f8c00f90c2044feca0ee1d3cc722185",
+    "url": "/react-deploy-22-12-21/static/media/user.9f8c00f9.jpeg"
+  },
+  {
+    "revision": "f394da7b940450af7558af0601f7c9f8",
+    "url": "/react-deploy-22-12-21/static/media/zoom.f394da7b.png"
+  }
+]);
